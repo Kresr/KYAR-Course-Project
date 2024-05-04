e715401acd67890e06b497b59c70fc26a0ba54c4
Kresr <justsk3l@mail.ru>
2024-05-05 02:04:43 +0300

    .

---
+++ xml/scriptochek.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('xml/onas.xml')
+  .then(function (response) {
+      return response.text();
+  })
+  .then(function (xmlDocText) {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(xmlDocText, "text/xml");
+      const MenuCards = xmlDoc.querySelectorAll('title');
+      const tableBody = document.querySelector("main");
+      MenuCards.forEach(function (student) {
+          const scr = student.querySelector("image").textContent;
+          const title = student.querySelector("zagolovok").textContent;
+          const text = student.querySelector("abzactz").textContent;
+          tableBody.innerHTML += `
+          <div>
+            <img src="${scr}" alt="" />
+            <h2>${title}</h2>
+            <p>
+              ${text}
+            </p>
+          </div>
+      `;
+      });
+  })
+  .catch(function (error) {
+      console.error('Error fetching or parsing XML:', error);
+  });
+});
